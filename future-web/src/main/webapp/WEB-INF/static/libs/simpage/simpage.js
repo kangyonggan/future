e454a0e4af31796d74d0c19c1d93b6ba0bb20231
kangyonggan <kangyonggan@gmail.com>
2017-09-15 09:53:00 +0800

    更换加载中图标

--- future-web/src/main/webapp/WEB-INF/static/libs/simpage/simpage.js
+++ future-web/src/main/webapp/WEB-INF/static/libs/simpage/simpage.js
@@ -36,39 +36,17 @@
     function simPage(contentArea, settings) {
         var $contentArea = $(contentArea);
         var $overlay = $();//empty set
-        var loadingTimer;
         // 把settings的属性合并到defaults，并且不改变defaults
         settings = $.extend(true, $.fn.simPage.defaults, settings);
         /**
          * 开始加载
          */
         function startLoading() {
-            // 清除定时器
-            clearInterval(loadingTimer);
             $overlay.remove();
-            $overlay = $('<div>' + settings.loadingText + '</div>').css({
-                position: "absolute",
-                left: 0,
-                top: 0,
-                bottom: 0,
-                right: 0,
-                zIndex: 9999,
-                textAlign: "center",
-                marginTop: "100px"
-            }).addClass("sim-page-loading");
+
+            $overlay = $('<div class="sim-page-loading"><span class="line1"></span> <span class="line2"></span> <span class="line3"></span> <span class="line4"></span> <span class="line5"></span> <span class="line6"></span> <span class="line7"></span> <span class="line8"></span> </div>');
+
             $contentArea.append($overlay);
-            var text = $overlay.html();
-            var count = 0;
-            // 定时输出正在加载...
-            loadingTimer = setInterval(function () {
-                var temp = ".";
-                for (var i = 0; i < count; i++) {
-                    temp += ".";
-                }
-                $overlay.html(text + temp);
-                count++;
-                count %= 3;
-            }, 1000);
         }
 
         /**
@@ -77,8 +55,6 @@
          * @param isSuccess
          */
         function stopLoading(isSuccess) {
-            // 清除定时器
-            clearInterval(loadingTimer);
             $overlay.remove();
             if (isSuccess) {
                 onSuccess();
@@ -201,8 +177,6 @@
         defaultUrl: '#index',
         // 内容URL（一般默认即可）
         contentUrl: getContentUrl,
-        // 加载中的文字
-        loadingText: '正在加载',
         // success回调方法
         success: onSuccess,
         // error回调方法
